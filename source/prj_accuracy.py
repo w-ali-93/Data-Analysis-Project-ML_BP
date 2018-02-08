21eeafbe886f9c2b0fa7ac07657dbaa37b43a320
w-ali-93 <35274857+w-ali-93@users.noreply.github.com>
2018-02-08 22:38:42 +0200

    Refactoring and optimization

--- source/prj_accuracy.py
+++ source/prj_accuracy.py
@@ -7,102 +7,96 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn import neighbors
-from sklearn import (manifold, datasets, decomposition, ensemble, discriminant_analysis, random_projection)
+from sklearn import manifold
 
 
 if __name__ == "__main__":
-    """ Load Raw data """
+    """Load data"""
     print("loading data ...")
     #Load the Training Data
-    data_path_x = "train_data.csv";
-    songs_data = pd.read_csv("train_data.csv", header=0);
+    train_data = pd.read_csv("train_data.csv", header=0);
     #Load the True Labels
-    data_path_y = "train_labels.csv";
-    songs_labels = pd.read_csv("train_labels.csv", header=0);
-    #Load the Test Data
-    data_path_z = "test_data.csv";
-    val_x = pd.read_csv("test_data.csv", header=0);
+    train_labels = pd.read_csv("train_labels.csv", header=0);
+    #Load the Test Data Used For Generating Predictions That Will Be Validated By Kaggle Checker
+    test_data = pd.read_csv("test_data.csv", header=0);
 
-    """ Pre process data"""
+    """Preprocess data"""
     print("preprocessing data ...")
-    scaler = preprocessing.StandardScaler().fit(songs_data)
+    scaler = preprocessing.StandardScaler().fit(train_data)
     scaler
     scaler.mean_
     scaler.scale_
-    scaler.transform(songs_data)
-    scaler.transform(val_x)
+    scaler.transform(train_data)
+    scaler.transform(test_data)
 
-    """ Partition data"""
+    """Partition data"""
     print("partitioning data ...")
-    train_x, test_x, train_y, test_y = train_test_split(songs_data, songs_labels, train_size=0.8, random_state=0);
+    train_x, test_x, train_y, test_y = train_test_split(train_data, train_labels, train_size=0.8, random_state=0);
 
-    """ Perform feature reduction"""
-    #Perform PCA on the Train Partition """
-    print("performing feature reduction ...")
-    train_x_pca = PCA(264)
-    train_x_pca.fit(train_x)
-    #Transform The Training Partition with the PCA"""
-    train_x_reduced = train_x_pca.transform(train_x)
-    test_x_reduced = train_x_pca.transform(test_x)
-    val_x_reduced = train_x_pca.transform(val_x)
+    """Perform feature reduction"""
+    # #Perform PCA on the Train Partition
+    # print("performing feature reduction ...")
+    # train_x_pca = PCA(264)
+    # train_x_pca.fit(train_x)
+    # #Transform The Training Partition with the PCA
+    # train_x = train_x_pca.transform(train_x)
+    # test_x = train_x_pca.transform(test_x)
+    # test_data = train_x_pca.transform(test_data)
 
     if 0:
         #http://www.astroml.org/book_figures/chapter7/fig_S_manifold_PCA.html
         #http://scikit-learn.org/stable/modules/manifold.html
-        """ Apply KNN with Modified LDE"""
-        print("applying Modified LDE ...")
+        """Apply KNN with LDE"""
+        print("applying LDE ...")
         n_neighbors = 22
         print("computing modified LLE embedding ...")
         prep = manifold.LocallyLinearEmbedding(n_neighbors, n_components=21, method='modified')
-        train_x_mlle = prep.fit_transform(train_x)
-        test_x_mlle = prep.fit_transform(test_x)
-        val_x_mlle = prep.fit_transform(val_x)
+        train_x = prep.fit_transform(train_x)
+        test_x = prep.fit_transform(test_x)
+        test_data = prep.fit_transform(test_data)
         print("Done. Reconstruction error: %g" % prep.reconstruction_error_)
 
-        """ Build the KNN model"""
-        print("building model...")
+        print("applying KNN ...")
         n_neighbors = 18
         weights = 'uniform'
-        clf = neighbors.KNeighborsClassifier(n_neighbors, weights)
-        clf.fit(train_x_mlle, np.asarray(train_y).ravel())
+        classif1 = neighbors.KNeighborsClassifier(n_neighbors, weights)
+        classif1.fit(train_x, np.asarray(train_y).ravel())
 
-        """ Output accuracy results for Train and Test partitions"""
-        print("Train Accuracy :: ", metrics.accuracy_score(train_y, clf.predict(train_x_mlle)));
-        print("Test Accuracy ::  ", metrics.accuracy_score(test_y, clf.predict(test_x_mlle)));
+        """Output accuracy results for Train and Test partitions"""
+        print("KNN Train Accuracy :: ", metrics.accuracy_score(train_y, classif1.predict(train_x)));
+        print("KNN Test Accuracy ::  ", metrics.accuracy_score(test_y, classif1.predict(test_x)));
 
-        """ Export the classification results to for the Validation set to a csv file """
-        test_data_results_knn = clf.predict(val_x_mlle)
+        """Predict the validation data and export predictions to CSV"""
+        test_data_results_knn = classif1.predict(test_data)
         df = pd.DataFrame(test_data_results_knn);
-        df.to_csv('songs_data_reduced.csv', index=False, header=False);
+        df.to_csv('test_data_results_knn.csv', index=False, header=False);
 
-    if 1:
-        """ Apply MLR"""
+    if 0:
+        """Apply MLR"""
         #apply multinomial logistic regression
         print("applying MLR ...")
-        mul_lr = linear_model.LogisticRegression(multi_class='multinomial', max_iter=1000, solver='newton-cg').fit(train_x, train_y.values.ravel());
-        print("**************")
-        print("Multinomial Logistic regression Train Accuracy :: ", metrics.accuracy_score(train_y, mul_lr.predict(train_x)));
-        print("Multinomial Logistic regression Test Accuracy ::  ", metrics.accuracy_score(test_y, mul_lr.predict(test_x)));
-        print("**************")
+        classif2 = linear_model.LogisticRegression(multi_class='multinomial', max_iter=1000, solver='newton-cg');
+        classif2.fit(train_x, train_y.values.ravel())
 
-        """ Predict the validation data using MLR and export predictions to CSV"""
-        # Classify the test data using multinomial logistic regression
-        test_data_results_mlr = mul_lr.predict(val_x)
+        print("Multinomial Logistic Regression Train Accuracy :: ", metrics.accuracy_score(train_y, classif2.predict(train_x)));
+        print("Multinomial Logistic Regression Test Accuracy ::  ", metrics.accuracy_score(test_y, classif2.predict(test_x)));
+
+        """Predict the validation data and export predictions to CSV"""
+        test_data_results_mlr = classif2.predict(test_data)
         df = pd.DataFrame(test_data_results_mlr);
         df.to_csv('test_data_results_mlr.csv', index=False, header=False);
 
-    if 0:
-        """ Apply OVR"""
+    if 1:
+        """Apply OVR"""
         #apply one vs rest classification
         print("applying OVR ...")
-        classif = linear_model.LogisticRegression(multi_class='ovr', max_iter=500, solver='newton-cg')
-        classif.fit(train_x, train_y.values.ravel())
-        print("**************")
-        print("One vs rest classification Train Accuracy :: ", metrics.accuracy_score(train_y, classif.predict(train_x)));
-        print("One vs rest classification Test Accuracy ::  ", metrics.accuracy_score(test_y, classif.predict(test_x)));
-        print("**************")
+        classif3 = linear_model.LogisticRegression(multi_class='ovr', max_iter=500, solver='newton-cg')
+        classif3.fit(train_x, train_y.values.ravel())
 
-        """ Predict the validation data using OVR and export predictions to CSV"""
-        test_data_results_ovr = classif.predict(val_x)
+        print("One vs Rest Classification Train Accuracy :: ", metrics.accuracy_score(train_y, classif3.predict(train_x)));
+        print("One vs Rest Classification Test Accuracy ::  ", metrics.accuracy_score(test_y, classif3.predict(test_x)));
+
+        """Predict the validation data and export predictions to CSV"""
+        test_data_results_ovr = classif3.predict(test_data)
         df = pd.DataFrame(test_data_results_ovr);
         df.to_csv('test_data_results_ovr.csv', index=False, header=False);
